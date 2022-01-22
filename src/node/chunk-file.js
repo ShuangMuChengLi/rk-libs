471be38aad2137093a0e3c45684bbba112cac923
黄武坚 <5674703+huang_wu_jian@user.noreply.gitee.com>
2022-01-22 18:00:53 +0800

    断点续传

--- src/node/chunk-file.js
+++ src/node/chunk-file.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const fs = require('fs');
 const { splitByParts, bufferMerge } = require('./split-chunk-merge');
-
-const filePath = path.resolve(__dirname, '1.mp4');
+const fileName = 'dist.zip';
+const filePath = path.resolve(__dirname, fileName);
 const targetPath = path.resolve(__dirname, './chunks');
 
-splitByParts(filePath, 3, targetPath, '1.mp4').then(res => {
+splitByParts(filePath, 3, targetPath, fileName).then(res => {
   // <file>-chunk-1
   // ...
   // <file>-chunk-3
--- src/node/merge-chunk.js
+++ src/node/merge-chunk.js
@@ -1,10 +1,10 @@
 const path = require('path');
 const { streamMerge } = require('./split-chunk-merge');
-
+const fileName = 'dist.zip';
 const inputPath = path.join(__dirname, 'chunks/');
-const outputPath = path.join(__dirname, 'chunks.mp4');
+const outputPath = path.join(__dirname, 'chunks-' + fileName);
 const inputPathList = new Array(3).fill().map((item, index) => {
-  return inputPath + '1.mp4-chunk-' + index;
+  return inputPath + fileName + '-chunk-' + index;
 });
 
 streamMerge(inputPathList, outputPath).then(res => {
