666dc136e4d2f10bd8ecd5d277843c3e92d10059
linchaoqun <lchaoqun@sina.cn>
2022-03-21 18:22:17 +0800

    测试

--- src/js/js-test.js
+++ src/js/js-test.js
@@ -1,11 +1,14 @@
-let p = function (){
-  return new Promise((resolve, reject)=>{
-    reject('reject');
-  });
+let o = {
+  name: 'o',
+  fn(...arg){
+    console.log(this.name, arg);
+  }
 };
-p().catch((data)=>{
-  console.log(1, data);
-  return 2;
-}).catch((data)=>{
-  console.log(2, data);
-});
+let q = {
+  name: 'q',
+  fn(...arg){
+    console.log(this.name, arg);
+  }
+};
+q.fn.apply(o, [1, 2]);
+q.fn.call(o, 1, 2);
---
+++ src/views/test/count-menu.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    test
+  </div>
+</template>
+
+<script>
+import {menu} from '../../../config/menu';
+export default {
+  name: 'VideoExample',
+  data() {
+    return {
+
+    };
+  },
+  mounted() {
+    let count = 0;
+    for(let item of menu){
+      if(item.children){
+        for(let sub of item.children){
+          count += sub.children.length;
+        }
+      }
+    }
+  }
+};
+</script>
+<style scoped lang="less">
+
+</style>
