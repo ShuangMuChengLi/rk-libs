1fac4439c42177675854ebda27150b04777cfcfc
linchaoqun <lchaoqun@sina.cn>
2022-10-24 15:42:31 +0800

    md5Sha  common.js版本

---
+++ src/js/math/md5-sha/md5-sha-common.js
@@ -0,0 +1,14 @@
+let { createHash } = require('crypto');
+/**
+ *
+ * @param message String 任意数据
+ * @param algorithm String md5 sha1 sha256 sha512
+ * @return {string} 十六进制的字符串
+ */
+function md5Sha(message, algorithm = 'md5'){
+  const hash = createHash(algorithm);
+  hash.update(message);
+  console.log(hash);
+  return hash.digest('hex');
+}
+module.exports = md5Sha;
---
+++ src/js/math/md5-sha/test.js
@@ -0,0 +1,3 @@
+let md5Sha = require('./md5-sha-common');
+console.log(md5Sha('123456', 'sha256'));
+console.log('8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92');
